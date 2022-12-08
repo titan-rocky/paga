36e1f0d838e83ce5792990a16a2fcc30e3f83b98
Albert <rockalbert466@gmail.com>
2022-12-08 23:52:44 +0530

    Create scascs.c

---
+++ templates/scascs.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+
+    int i,j;
+    const int end=2*n-1;
+    int arr[end]={0}*5;
+    
+
+
+    for (i=0;i<end;i++)
+    {
+        for (j=0;j<floor(end/2)+1;j++)
+        {
+
+        }
+    }
+
+}
